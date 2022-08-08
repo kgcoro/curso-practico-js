32de15e64b5346cef6d309b2ec9ef90cf3cf3757
Kleber Coro <kgcoro@gmail.com>
2022-08-08 15:50:01 -0500

    addEventListener

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const form = document.querySelector('#form')
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result')
 
-console.log(input.value);
+// form.addEventListener('submit', sumarInputValues);
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
+// function sumarInputValues() {
+//     event.preventDefault();
+//     const valSum = parseInt(input1.value) + parseInt(input2.value);
+//     pResult.innerText = "Resultado " + valSum;
+// }
 
-h1.innerHTML = 'Patito <br> Feo';
-h1.innerText = 'Patito <br> Feo';
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo')
+btn.addEventListener('click', sumarInputValues);
 
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-
-input.value = "465";
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://cafans.b-cdn.net/images/Category_63376/subcat_221960/BdhMO6h5_1312211059461gpadd.jpeg');
-
-pid.innerHTML = "";
-pid.appendChild(img);
+function sumarInputValues(event) {
+    // event.preventDefault();
+    const valSum = parseInt(input1.value) + parseInt(input2.value);
+    pResult.innerText = "Resultado " + valSum;
+}
